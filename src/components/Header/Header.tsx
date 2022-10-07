9cd960ef22689926ee0a8db92ce03f304910f663
NguyenChungDeveloper <matthewricharddev@gmail.com>
2022-10-07 11:59:09 +0700

    feat(select-multi-wallet): improve select fewcha wallet

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -15,6 +15,7 @@ import { useSelector } from "react-redux";
 import Modal from "react-modal";
 import { Title3 } from "../common/StyledComponent";
 import icon_close from "../../assets2/image/icon_close.svg";
+import icon_check from "../../assets2/image/icon_check.svg";
 import logo_fewcha from "../../assets2/image/logo_fewcha.jpg";
 import logo_martian from "../../assets2/image/logo_martian.jpg";
 import logo_petra from "../../assets2/image/logo_petra.jpg";
@@ -25,9 +26,13 @@ import icon_arrow_right_exit from "../../assets2/image/icon_arrow_right_exit.svg
 
 import styled from "styled-components";
 import { toast, ToastOptions } from "react-toastify";
+import { AnyArray } from "immer/dist/internal";
+import cn from "services/cn";
 
 const ListWallet = styled.aside``;
 const WalletItem = styled.div``;
+
+type NameWallet = "fewcha" | "petra" | "martian" | "";
 
 const Header: React.FC<{
   wallet: Aptos;
@@ -50,12 +55,13 @@ const Header: React.FC<{
 
   const options: ToastOptions<{}> = {
     position: "top-right",
-    autoClose: 5000,
+    autoClose: 1500,
     hideProgressBar: false,
     closeOnClick: true,
     pauseOnHover: true,
     draggable: true,
     progress: undefined,
+    type:'success'
   };
   const [showMobile, setShowMobile] = useState(false);
   const [balance, setBalance] = useState("0");
@@ -64,6 +70,8 @@ const Header: React.FC<{
   const [isConnectFewchaWallet, setIsConnectFewchaWallet] =
     useState<boolean>(false);
   const [showMore, setShowMore] = useState<boolean>(false);
+
+  const [nameCurrentWallet, setNameCurrentWallet] = useState<NameWallet>("");
 
   const toggleMobile = () => {
     setShowMobile(!showMobile);
@@ -84,16 +92,20 @@ const Header: React.FC<{
 
   // console.log("balance--: ", balance);
 
-
   // ----> CONNECT FEWCHA WALLET ---->
   const handleConnectFewchaWallet = async () => {
     console.log("------> CONNECT FEWCHA =>>");
     try {
+      if (isConnectFewchaWallet) {
+        toast("You are currently connected to this wallet", options);
+        return;
+      }
       const res = await wallet.action.connect();
       console.log("res: ", res);
       if (res.status === 200 && res.data.address) {
         setIsConnectFewchaWallet(true);
         setAddressFewchaWallet(res.data.address);
+        setNameCurrentWallet("fewcha");
         setTimeout(() => {
           setShowModal(false);
         }, 1000);
@@ -102,9 +114,6 @@ const Header: React.FC<{
       console.log("Error: ", error);
     }
   };
-  useEffect(() => {
-    console.log("addressFewchaWallet: ", addressFewchaWallet);
-  }, [isConnectFewchaWallet]);
 
   // ----> DISCONNECT FEWCHA WALLET ---->
   const handleDisConnectFewchaWallet = async () => {
@@ -115,6 +124,7 @@ const Header: React.FC<{
       if (response.data) {
         setIsConnectFewchaWallet(false);
         setAddressFewchaWallet("");
+        setNameCurrentWallet("");
       }
     } catch (error) {
       console.log(error);
@@ -133,6 +143,10 @@ const Header: React.FC<{
   const handleShowDialogWallet = () => {
     setShowModal(true);
   };
+  function handleSlectOtherWallet(e: any) {
+    e.stopPropagation();
+    setShowModal(true);
+  }
   function afterOpenModal() {
     // references are now sync'd and can be accessed.
   }
@@ -145,11 +159,18 @@ const Header: React.FC<{
     if (isConnectFewchaWallet) {
       return (
         <div>
-          <div className="flex justify-between gap-x-2 items-center relative shadow ">
+          <div className="flex justify-between gap-x-2 items-center relative mb-1">
             {addressFewchaWallet && (
-              <p className="font-bold">
-                {truncateEthAddress(addressFewchaWallet)}
-              </p>
+              <div className="flex items-center gap-x-1.5 pl-1">
+                <img
+                  src={logo_fewcha}
+                  alt="Fewcha Wallet"
+                  className="w-6 h-6 rounded-full"
+                />
+                <p className="font-bold">
+                  {truncateEthAddress(addressFewchaWallet)}
+                </p>
+              </div>
             )}
             <img
               src={showMore ? icon_chevron_up : icon_chevron_down}
@@ -159,13 +180,31 @@ const Header: React.FC<{
               onClick={() => setShowMore((prev) => !prev)}
             />
           </div>
+
           {showMore && (
             <div
               onClick={handleDisConnectFewchaWallet}
-              className="flex gap-x-1 items-center hover:cursor-pointer absolute font-bold text-red-500 bg-[#34495e] max-w-max px-3 rounded min-h-[64px] shadow"
+              className="text-white hover:cursor-pointer absolute font-bold  bg-[#34495e] max-w-max px-3 rounded min-h-[64px] shadow py-2"
             >
-              <p>Disconnect</p>
-              <img src={icon_arrow_right_exit} alt="Disconnect" />
+              <article className="flex gap-x-2 mb-1 items-center hover:text-red-500">
+                <p>Disconnect</p>
+                {/* <img src={icon_arrow_right_exit} alt="Disconnect" /> */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 576 512"
+                  width="18px"
+                  height="18px"
+                  fill="currentColor"
+                >
+                  <path d="M534.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L434.7 224 224 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128zM192 96c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0c-53 0-96 43-96 96l0 256c0 53 43 96 96 96l64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l64 0z" />
+                </svg>
+              </article>
+              <p
+                onClick={(e) => handleSlectOtherWallet(e)}
+                className="py-1 hover:text-blue-500"
+              >
+                Select Other Wallet
+              </p>
             </div>
           )}
         </div>
@@ -222,7 +261,10 @@ const Header: React.FC<{
 
               <ListWallet className="mt-3 uppercase font-medium">
                 <WalletItem
-                  className="py-3 flex items-center gap-x-3.5 mb-2 px-4 rounded-md hover:cursor-pointer hover:bg-[#ffcccc] hover:text-[#17c0eb]"
+                  className={cn(
+                    "py-3 flex items-center gap-x-3.5 mb-2 px-4 rounded-md hover:cursor-pointer hover:bg-[#ffcccc] hover:text-[#17c0eb]",
+                    {}
+                  )}
                   onClick={handleConnectFewchaWallet}
                 >
                   <img
@@ -230,7 +272,14 @@ const Header: React.FC<{
                     alt="Fewcha Aptos Wallet"
                     className="w-8 h-8 rounded-full"
                   />
+
                   <p>Fewcha Aptos Wallet</p>
+                  {isConnectFewchaWallet && (
+                    <div className="flex gap-x-8 items-center">
+                      <span className="text-sm italic">(connected)</span>
+                      <img src={icon_check} alt="Connected" />
+                    </div>
+                  )}
                 </WalletItem>
 
                 <WalletItem
