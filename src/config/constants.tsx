6bd2d6f448f88be237b3dea6733279589ca93857
NguyenChungDeveloper <matthewricharddev@gmail.com>
2022-10-12 15:19:46 +0700

    feat(feature/verify-social): Build done UI of Model Social

--- src/config/constants.tsx
+++ src/config/constants.tsx
@@ -1,3 +1,8 @@
+import logo_discord from "../../src/assets2/image/discord.svg";
+
+import logo_twitter from "../../src/assets2/image/twitter.svg";
+
+
 interface MenuItemType {
   path: string;
   label: string;
@@ -11,5 +16,40 @@ export const MENUS: MenuItemType[] = [
   {
     label: "Event",
     path: "/event",
+  },
+];
+
+export const customStylesModal = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    background: "white",
+    padding: "24px",
+    borderRadius: "14px",
+  },
+};
+
+interface ItemSocial {
+  id: number;
+  name: string;
+  des: string;
+  icon: any;
+}
+export const ListSocial: ItemSocial[] = [
+  {
+    id: 1,
+    name: "Discord",
+    des: "Verify Discord",
+    icon: logo_discord,
+  },
+  {
+    id: 2,
+    name: "Twitter",
+    des: "Verify Twitter",
+    icon: logo_twitter,
   },
 ];
