4e6ae9ebd1fe6e5f0468d098f3656270d3ce3038
NguyenChungDeveloper <matthewricharddev@gmail.com>
2022-10-19 15:44:06 +0700

    select-network-ui

---
+++ src/components/network/network.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Network:React.FC = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Network
--- src/feature/network/network-slice.ts
+++ src/feature/network/network-slice.ts
@@ -1,28 +1,30 @@
+import { RootState } from './../../components/App/store';
+import { NetworkItem } from "./../../components/App/type";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-export interface NETWORK {
-  name: string | null;
-  url: string;
-}
-const initialState: NETWORK = {
-  name: "",
-  url: "",
+const initialState: NetworkItem = {
+  type: "Testnet",
+  label: "Aptos",
+  url: "https://fullnode.devnet.aptoslabs.com",
 };
 const networkSlice = createSlice({
   name: "network",
   initialState,
   reducers: {
-    updateNetwork(state, action: PayloadAction<NETWORK>) {
-      state.name = action.payload.name;
+    updateNetwork(state, action: PayloadAction<NetworkItem>) {
+      state.type = action.payload.type;
+      state.label = action.payload.label;
       state.url = action.payload.url;
     },
 
     reset(state) {
-      state.name = null;
-      state.url = "";
+      state.type = "Testnet";
+      state.label = "Aptos";
+      state.url = "https://fullnode.devnet.aptoslabs.com";
     },
   },
 });
 export const { updateNetwork, reset } = networkSlice.actions;
+export const selectNetwork = (state:RootState) => state.network;
 const networkReducer = networkSlice.reducer;
 export default networkReducer;
